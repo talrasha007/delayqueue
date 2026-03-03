679a77471239db78d3d15e7602e02bc2efb556e5
Tal <talrasha007@gmail.com>
2026-03-03 19:51:18 +0800

    use heap.

--- delayqueue.go
+++ delayqueue.go
@@ -1,6 +1,7 @@
 package delayqueue
 
 import (
+	"container/heap"
 	"errors"
 	"sync"
 	"time"
@@ -18,25 +19,55 @@ const (
 type Task func()
 
 type delayedQueue struct {
-	mu      sync.Mutex
-	cond    *sync.Cond
-	queue   []scheduledTask
-	cap     int
-	policy  OverflowPolicy
-	running bool
+	mu     sync.Mutex
+	cond   *sync.Cond
+	queue  taskHeap
+	cap    int
+	policy OverflowPolicy
+	wake   chan struct{}
+	seq    uint64
 }
 
 type scheduledTask struct {
 	at   time.Time
 	task Task
+	seq  uint64
+}
+
+type taskHeap []scheduledTask
+
+func (h taskHeap) Len() int { return len(h) }
+
+func (h taskHeap) Less(i, j int) bool {
+	if h[i].at.Equal(h[j].at) {
+		return h[i].seq < h[j].seq
+	}
+	return h[i].at.Before(h[j].at)
+}
+
+func (h taskHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *taskHeap) Push(x any) {
+	*h = append(*h, x.(scheduledTask))
+}
+
+func (h *taskHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	old[n-1] = scheduledTask{}
+	*h = old[:n-1]
+	return x
 }
 
 func New(cap int, policy OverflowPolicy) *delayedQueue {
 	q := &delayedQueue{
 		cap:    cap,
 		policy: policy,
+		wake:   make(chan struct{}, 1),
 	}
 	q.cond = sync.NewCond(&q.mu)
+	heap.Init(&q.queue)
 	go q.loop()
 	return q
 }
@@ -45,10 +76,16 @@ func (q *delayedQueue) Add(d time.Duration, task Task) error {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
+	st := scheduledTask{
+		at:   time.Now().Add(d),
+		task: task,
+		seq:  q.seq,
+	}
+	q.seq++
+
 	// 队列未满
 	if len(q.queue) < q.cap {
-		q.push(scheduledTask{at: time.Now().Add(d), task: task})
-		q.cond.Signal()
+		q.push(st)
 		return nil
 	}
 
@@ -58,16 +95,14 @@ func (q *delayedQueue) Add(d time.Duration, task Task) error {
 		for len(q.queue) >= q.cap {
 			q.cond.Wait()
 		}
-		q.push(scheduledTask{at: time.Now().Add(d), task: task})
-		q.cond.Signal()
+		q.push(st)
 		return nil
 
 	case DropOldest:
 		// 删除最早未执行
-		q.queue[0] = scheduledTask{}
-		q.queue = q.queue[1:]
-		q.push(scheduledTask{at: time.Now().Add(d), task: task})
-		q.cond.Signal()
+		heap.Pop(&q.queue)
+		q.push(st)
+		q.notifyWake()
 		return nil
 	}
 
@@ -75,7 +110,19 @@ func (q *delayedQueue) Add(d time.Duration, task Task) error {
 }
 
 func (q *delayedQueue) push(st scheduledTask) {
-	q.queue = append(q.queue, st)
+	shouldWake := len(q.queue) == 0 || st.at.Before(q.queue[0].at)
+	heap.Push(&q.queue, st)
+	q.cond.Signal()
+	if shouldWake {
+		q.notifyWake()
+	}
+}
+
+func (q *delayedQueue) notifyWake() {
+	select {
+	case q.wake <- struct{}{}:
+	default:
+	}
 }
 
 func (q *delayedQueue) loop() {
@@ -85,27 +132,24 @@ func (q *delayedQueue) loop() {
 			q.cond.Wait()
 		}
 
-		now := time.Now()
-		// 找最先要执行的任务
-		minIdx := -1
-		var mint time.Time
-		for i, t := range q.queue {
-			if minIdx == -1 || t.at.Before(mint) {
-				minIdx = i
-				mint = t.at
-			}
-		}
-
-		wait := mint.Sub(now)
+		wait := time.Until(q.queue[0].at)
 		if wait > 0 {
 			q.mu.Unlock()
-			time.Sleep(wait)
+			timer := time.NewTimer(wait)
+			select {
+			case <-timer.C:
+			case <-q.wake:
+				if !timer.Stop() {
+					select {
+					case <-timer.C:
+					default:
+					}
+				}
+			}
 			continue
 		}
 
-		st := q.queue[minIdx]
-		// 从队列删除
-		q.queue = append(q.queue[:minIdx], q.queue[minIdx+1:]...)
+		st := heap.Pop(&q.queue).(scheduledTask)
 		q.cond.Signal() // notify producers if blocked
 		q.mu.Unlock()
 
